3d0f9635ad20fb1801c5ff21d6820f7494f7c6d9
Nannan <1041836312@qq.com>
2021-04-08 02:51:54 +0800

    centos6.10 使用服务端修复插件

--- test.sh
+++ test.sh
@@ -1,3 +1,94 @@
+docker pull centos:6.10
+rm -rf /root/centos
+docker run -itd --name centos6.1 --privileged=true -v /root/centos/root:/root -v /root/centos/neople:/home/neople --net=host --memory=8g --oom-kill-disable --shm-size=8g centos:6.10
+docker exec -it centos6.1 /bin/bash
+
+
+#curl https://www.getpagespeed.com/files/centos6-eol.repo --output /etc/yum.repos.d/CentOS-Base.repo
+#yum makecache
+
+
+#wget https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbWFZUW41a21yRnFoSVJxX21lR3ZMN1R1eGZCdEE=.tar.gz
+#mv aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbWFZUW41a21yRnFoSVJxX21lR3ZMN1R1eGZCdEE\=.tar.gz jjg.tar.gz
+#tar -zxvf jjg.tar.gz -C /root/dofServer/neople/game/
+
+
+#yum downgrade -y libselinux
+#yum install yum-download
+#yum install --downloadonly --downloaddir=/root/rpm libXtst xulrunner
+
+
+yum install yum-utils -y
+#yumdownloader --resolve --destdir /root/rpm libXtst
+#yumdownloader --resolve --destdir /root/rpm xulrunner
+yumdownloader --resolve --destdir /root/rpm glibc.i686
+yumdownloader --resolve --destdir /root/rpm libstdc++
+yumdownloader --resolve --destdir /root/rpm zlib
+yumdownloader --resolve --destdir /root/rpm createrepo
+
+#yumdownloader --resolve --destdir /root/rpm glibc.i686
+#yumdownloader --resolve --destdir /root/rpm GeoIP-devel.i686
+#yumdownloader --resolve --destdir /root/rpm libstdc++-devel.i686
+#yumdownloader --resolve --destdir /root/rpm zlib-devel.i686
+#yumdownloader --resolve --destdir /root/rpm psmisc
+#yumdownloader --resolve --destdir /root/rpm libaio
+#yumdownloader --resolve --destdir /root/rpm libnuma*
+#yumdownloader --resolve --destdir /root/rpm numactl
+#yumdownloader --resolve --destdir /root/rpm createrepo
+
+777
+cd /root/rpm
+rpm -ivh libxml2-python-2.6.26-2.1.25.el5_11.x86_64.rpm
+rpm -ivh createrepo-0.4.11-3.el5.noarch.rpm
+
+createrepo /root/rpm
+#cp /etc/yum.repos.d/CentOS-Media.repo /root/CentOS-Media.repo
+
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+mv /etc/yum.repos.d/CentOS-Media.repo /etc/yum.repos.d/CentOS-Media.repo.bak
+cp /root/CentOS-Media.repo /etc/yum.repos.d/CentOS-Media.repo
+
+
+#yum -y install libXtst xulrunner
+
+yum -y install glibc.i686
+#yum -y install libstdc++
+yum -y install libstdc++.i686
+#yum -y install zlib
+yum -y install zlib-1.2.3-29.el6.i686
+
+#yum -y install psmisc
+#yum -y install libaio
+#yum -y install libnuma*
+#yum -y install numactl
+#yum -y install  libstdc++
+#yum -y install GeoIP-devel.i686
+#centos7 libstdc++-4.8.5-44.el7.i686
+mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
+mv /etc/yum.repos.d/CentOS-Media.repo.bak /etc/yum.repos.d/CentOS-Media.repo
+
+sed -i "s/__IP__/192.168.2.111/g" `find /home/neople -type f -name "*.cfg"`
+sed -i "s/mysql_port/3306/g" `find /home/neople -type f -name "*.cfg"`
+#密码若有特殊字符需要转义
+sed -i "s/mysql_pwd_o/uu5\!\^\%jg/g" `find /home/neople -type f -name "*.cfg"`
+sed -i "s/mysql_pwd/20e35501e56fcedbe8b10c1f8bc3595be8b10c1f8bc3595b/g" `find /home/neople -type f -name "*.cfg"`
+sed -i "s/mysql_acc/game/g" `find /home/neople -type f -name "*.cfg"`
+sed -i "s/mysql_ip/127.0.0.1/g" `find /home/neople -type f -name "*.cfg"`
+
+echo -e "\033[32m 服务器配置\033[0m"
+cat /home/neople/bridge/cfg/bridge.cfg
+
+#rpm -Uvh --force --nodeps *.rpm
+#rpm -ivh *.rpm --force --nodeps
+
+
+##Cannot access the Hardware Clock via any known method.
+##Use the --debug option to see the details of our search for an access method. 输入 tzselect
+
+
+
+
+
 docker pull centos:centos7
 docker run -itd --name centos7 --privileged=true -v /root/centos/root:/root -v /root/centos/neople:/home/neople --net=host --memory=8g --oom-kill-disable --shm-size=8g centos:centos7
 上传nnn.tar.gz到/root/centos/root目录中
@@ -99,8 +190,9 @@ docker exec -it centos5 /bin/bash
 #wget https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbWFZUW41a21yRnFoSVJyYnBMSGlYSEROYWdHRGc/ZT1oQjd3Mm8=.tar.gz
 tar -zxvf /root/nnn.tar.gz -C /
 tar -kzxvf /root/nnn.tar.gz -C /
-#wget https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbWFZUW41a21yRnFoSVJxX21lR3ZMN1R1eGZCdEE=.tar.gz
-#tar -zxvf g.tar.gz -C /root/dofServer/neople/game/
+wget https://link.jscdn.cn/1drv/aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbWFZUW41a21yRnFoSVJxX21lR3ZMN1R1eGZCdEE=.tar.gz
+mv aHR0cHM6Ly8xZHJ2Lm1zL3UvcyFBbWFZUW41a21yRnFoSVJxX21lR3ZMN1R1eGZCdEE\=.tar.gz jjg.tar.gz
+tar -zxvf jjg.tar.gz -C /root/dofServer/neople/game/
 sed -i -e 's/^#baseurl=/baseurl=/' -e 's/^mirrorlist=/#mirrorlist=/' -e 's!http://mirror.centos.org/centos/$releasever/!http://mirrors.tuna.tsinghua.edu.cn/centos-vault/5.11/!' /etc/yum.repos.d/*.repo && sed -i -e 's/enabled=1/enabled=0/' /etc/yum.repos.d/libselinux.repo /etc/yum/pluginconf.d/fastestmirror.conf
 
 #yum downgrade -y libselinux
@@ -139,19 +231,20 @@ mv /etc/yum.repos.d/CentOS-Media.repo /etc/yum.repos.d/CentOS-Media.repo.bak
 cp /root/CentOS-Media.repo /etc/yum.repos.d/CentOS-Media.repo
 
 
-#yum -y install libXtst xulrunner
+yum -y install libXtst xulrunner
 
 yum -y install glibc.i686
 yum -y install libstdc++
-
 yum -y install zlib-devel.i386
-#yum -y install psmisc
-#yum -y install libaio
-#yum -y install libnuma*
-#yum -y install numactl
-#yum -y install  libstdc++
-#yum -y install GeoIP-devel.i686
+yum -y install zlib-1.2.3-29.el6.i686
 
+yum -y install psmisc
+yum -y install libaio
+yum -y install libnuma*
+yum -y install numactl
+yum -y install  libstdc++
+yum -y install GeoIP-devel.i686
+#centos7 libstdc++-4.8.5-44.el7.i686
 mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
 mv /etc/yum.repos.d/CentOS-Media.repo.bak /etc/yum.repos.d/CentOS-Media.repo
 
